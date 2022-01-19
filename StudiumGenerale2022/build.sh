f0d8fa8564cd26cbb18637a9f790188a23728d4d
Ethan Weed <weed.ethan@gmail.com>
2022-01-19 11:29:56 +0100

    auto-updated with build.sh

--- StudiumGenerale2022/build.sh
+++ StudiumGenerale2022/build.sh
@@ -2,9 +2,40 @@
 
 # build html documents
 
-find ./ -iname "*.md" -type f -exec sh -c 'pandoc "${0}" -o "./output/$(basename ${0%.md}.html)"' {} \;
 
-#pandoc --standalone /Users/ethan/Documents/GitHub/ExPsyLing/2021/Planning/Syllabus.md -o /Users/ethan/Documents/GitHub/ExPsyLing/2021/Planning/Syllabus.html
+# convert course information docs to html
+cd /Users/ethan/Documents/GitHub/Studium_Generale/StudiumGenerale2022/CourseInfo
+find ./ -iname "*.md" -type f -exec sh -c 'pandoc "${0}" -o "./html/$(basename ${0%.md}.html)"' {} \;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Push to github
+
+git add -A
+git commit -m "auto-updated with build.sh"
+git push origin master
+
+
+
+
+
+
+
+
+
+
+#pandoc --standalone /Users/ethan/Documents/GitHub/ExPsyLing/2021/Planning/Syllabus.md -o #/Users/ethan/Documents/GitHub/ExPsyLing/2021/Planning/Syllabus.html
 
 
 # in case you need it:
@@ -16,7 +47,7 @@ find ./ -iname "*.md" -type f -exec sh -c 'pandoc "${0}" -o "./output/$(basename
 ####### Lecture slides
 
 # Lecture 1
-#pandoc -t revealjs -s -o /Users/ethan/Documents/GitHub/ExPsyLing/2021/Slides/Lecture_02_Intro.html /Users/ethan/Documents/GitHub/ExPsyLing/2021/Slides/Lecture_02_Intro.md -V theme=solarized -V revealjs-url=https://unpkg.com/reveal.js/
+#pandoc -t revealjs -s -o /Users/ethan/Documents/GitHub/ExPsyLing/2021/Slides/Lecture_02_Intro.html #/Users/ethan/Documents/GitHub/ExPsyLing/2021/Slides/Lecture_02_Intro.md -V theme=solarized -V revealjs-url=https://unpkg.com/reveal.js/
 
 
 
@@ -24,9 +55,9 @@ find ./ -iname "*.md" -type f -exec sh -c 'pandoc "${0}" -o "./output/$(basename
 
 
 # Other
-#pandoc --standalone /Users/ethan/Documents/GitHub/ExPsyLing/2021/Planning/Syllabus.md -o /Users/ethan/Documents/GitHub/ExPsyLing/2021/Planning/Syllabus.html
+#pandoc --standalone /Users/ethan/Documents/GitHub/ExPsyLing/2021/Planning/Syllabus.md -o #/Users/ethan/Documents/GitHub/ExPsyLing/2021/Planning/Syllabus.html
 
-#pandoc --standalone /Users/ethan/Documents/GitHub/ExPsyLing/2021/Planning/Schedule.md -o /Users/ethan/Documents/GitHub/ExPsyLing/2021/Planning/Schedule.html
+#pandoc --standalone /Users/ethan/Documents/GitHub/ExPsyLing/2021/Planning/Schedule.md -o #/Users/ethan/Documents/GitHub/ExPsyLing/2021/Planning/Schedule.html
 
 # Push to github
 
