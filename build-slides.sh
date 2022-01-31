77745630f6066ce2b4da4b06da1b1d28b51cde85
Ethan Weed <weed.ethan@gmail.com>
2022-01-31 13:24:59 +0100

    auto-updated with build.sh

--- build-slides.sh
+++ build-slides.sh
@@ -4,10 +4,11 @@
 ## Lecture Slides
 
 cd /Users/ethan/Documents/GitHub/Studium_Generale/StudiumGenerale2022/Slides
-for i in *.md ; do echo "$i" && pandoc metadata.yaml -s -t revealjs -s -V revealjs-url=https://unpkg.com/reveal.js/ --include-in-header=styles.css $i -o html/$i.html ; done
+for i in *.md ; do echo "$i" && pandoc metadata.yaml -s -t revealjs -s -V revealjs-url=https://unpkg.com/reveal.js/ --include-in-header=styles.css $i -o html/$i{0%.md}.html ; done
 
 # Push to github
 
 git add -A
 git commit -m "auto-updated with build.sh"
 git push origin master
+
