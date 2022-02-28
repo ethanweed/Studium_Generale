c62ac8003dc6d5d7c1dfb46907347ae3c9f1dadc
Ethan Weed <weed.ethan@gmail.com>
2022-02-28 14:17:08 +0100

    auto-updated with build-mermaid.sh

--- build-mermaid.sh
+++ build-mermaid.sh
@@ -4,10 +4,16 @@
 
 cd /Users/ethan/Documents/GitHub/Studium_Generale/StudiumGenerale2022/Slides/Images
 
-for i in *.md ; do mmdc -i "$i" -o "${i%.*}.jpg" -b transparent; done
+for i in *.md ; do mmdc -i "$i" -o "${i%.*}.png" -b transparent; done
+
+for i in *.png ; do convert "$i" "${i%.*}.jpg" ; done
 
 # Push to github
 
 git add -A
 git commit -m "auto-updated with build-mermaid.sh"
 git push origin master
+
+
+
+for i in *.jpeg; do sips -s format png -s formatOptions 100 "${i}" --out "${i%jpeg}png"; done
